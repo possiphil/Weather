5fea33dfd2d9990289a41145833024d2a90ca179
Philipp <ph2003@gmx.net>
2023-06-28 12:54:47 +0200

    Initial commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Philipp Sanktjohanser on 05.12.22.
+//
+// https://www.youtube.com/watch?v=b8sP7AS0CAY
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
